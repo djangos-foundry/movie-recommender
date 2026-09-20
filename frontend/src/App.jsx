c05fa02ab73034daf562b6100026f7fa93176750
snehal-sahu-98 <snehalsahu.jb1998@gmail.com>
2026-09-20 23:17:52 +0530

    CineTrack v0.6 -updated movie title page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -627,6 +627,7 @@ export default function App() {
           cardSize={cardSize}
           onCardSizeChange={setCardSize}
           shortcuts={shortcuts}
+          onRemoveItem={handleRemoveItem}
         />
       )}
 
